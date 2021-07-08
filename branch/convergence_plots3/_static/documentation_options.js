2921877f439af4d76949442cca8b54c383b83de7
atharva-2001 <atharva-2001@users.noreply.github.com>
2021-07-08 08:20:57 +0000

    deploy: 91bc3df826735205a825f9f4863a9cf3a8b12c4e

--- branch/convergence_plots3/_static/documentation_options.js
+++ branch/convergence_plots3/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.dev4066',
+    VERSION: '3.0.dev4065',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

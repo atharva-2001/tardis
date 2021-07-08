a026c5c2299d1b45dd3511c45d0a506b61707b30
atharva-2001 <atharva-2001@users.noreply.github.com>
2021-07-08 07:47:04 +0000

    deploy: 29706dc1462adcc18917ce5214a80a408b09b836

--- branch/sdec_docs/_static/documentation_options.js
+++ branch/sdec_docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.dev4020',
+    VERSION: '3.0.dev4021',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

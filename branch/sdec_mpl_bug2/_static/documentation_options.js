707a9f8f8a893867182af9e9d433552e07c23e87
atharva-2001 <atharva-2001@users.noreply.github.com>
2021-11-23 16:20:57 +0000

    deploy: 164d44126b89b408b7a14e8a0935a4d92bfa94dd

---
+++ branch/sdec_mpl_bug2/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '2021.11.23.0.dev102+g164d4412',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
